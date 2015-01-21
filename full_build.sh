78f55f0a6bf754ff450ed8dbade01212f4083a55
WIDEN_SVN <zibets.han@gmail.com>
2015-01-21 14:07:11 +0900

    modify file name fix full_build.sh

--- full_build.sh
+++ full_build.sh
@@ -7,4 +7,4 @@ make ARCH=arm CROSS_COMPILE=arm-linux-gnueabi- bcmrpi_defconfig
 make ARCH=arm CROSS_COMPILE=arm-linux-gnueabi- -j5
 make ARCH=arm CROSS_COMPILE=arm-linux-gnueabi- modules
 export MODULES_PATH=./make_modules
-make ARCH=arm CROSS_COMPILE=arm-linux-gnueabi INSTALL_MOD_PATH=${MODULES_PATH} modules_install 
+make ARCH=arm CROSS_COMPILE=arm-linux-gnueabi- INSTALL_MOD_PATH=${MODULES_PATH} modules_install 
