32c8a5a0a165b1fc78b8b375ec06a7fabbe25036
jihye <wlgp12345@gmail.com>
2015-01-22 22:38:02 +0900

    ongoing

--- net/stbm/ethtsyn.c
+++ net/stbm/ethtsyn.c
@@ -7,6 +7,19 @@
 
 bool EthTSynHardwareTimestampSupport;
 
+Std_ReturnType currentTime;
+Std_ReturnType currentTimeRaw;
+Std_ReturnType timeDifferenceOfCurrentTimeRaw;
+
+StbM_SynchronizedTimeBaseType timeBaseId;
+StbM_TimeStampType*	timeStampPtr;
+StbM_UserDataType* userDataPtr;
+StbM_TimeStampRawType* timeStampRawPtr;
+StbM_TimeStampRawType givenTimeStamp;
+StbM_TimeStampRawType* timeStampDiffPtr;
+
+
+
 /* Initialize all internal variables and set the EthTSync module to init state */
 void 		EthTSyn_Init(const EthTSyn_ConfigType* configPtr) {
   //When DET reporting is enabled EthTSyn module shall call DEt_ReportError() with the error code
@@ -28,6 +41,7 @@ void 		EthTSyn_GetVersionInfo(Std_VersionInfoType* versioninfo) {
 Std_ReturnType 	EthTSyn_GetCurrentTime(StbM_SynchronizedTimeBaseType timeBaseId,
 				       StbM_TimeStampType* timeStampPtr,
 				       EthTSyn_SyncStateType* syncState) {
+  // EthIf_GetCurrentTime(
   return E_OK;
 }
 
@@ -52,27 +66,30 @@ void		EthTSyn_RxIndication(uint8_t CtrlIdx,
 
 void		EthTSyn_TxConfirmation(uint8_t CtrlIdx,
 				       uint8_t BufIdx) {
-   if(EthTSynHardwareTimestampSupport == true) {
-      /* the egress time stamp shall be retrieved for Pdelay_Req and Pdelay_Resp from the EthIf */
-      // EthIf_GetEgressTimeStamp(uint8_t, uint8_t, Eth_TimeStampQualType*, Eth_TimeStampType*);
-   } else {    // In case EthTSynHardwareTimestamp is set to FALSE
-      if(Type == Sync || Type == Pdelay_Resp) {
-         // StbM_GetCurrentTime(StdM_ReturnType, StbM_SynchronizedTimeBaseType, StbM_TimeStampType*, StbM_UserDataType*);
-      } else if(Type == Pdelay_Req) {
-         // StbM_GetCurrentTimeRaw(Std_ReturnType, Std_TimeStampRawType*);
-         // T1 = *timeStampRawPtr;
-      } else if(Type == Pdelay_Resp) {
-         // givenTimeStamp = T2;
-         // StbM_GetCurrentTimeDiff(Std_ReturnType, Std_TimeStampRawType, StbM_TimeStampRawType*);
-         // timeStampDiffPtr = (T3-T2)    /* One part of D = ((T4-T1) - (T3-T2)) / 2 */
-      } else if(Type == Sync && Type == EthTimeGatewayMasterPort) {
-         // givenTimeStamp = (Tr,i);   // Maybe 'Tr' means the time that received message, and 'i' means time-aware system indexed i
-         // StbM_GetCurrentTimeDiff(Std_ReturnType, StbM_TimeStampRawType, StbM_TimeStampRawType*)
-         // timeStampDiffPtr = (Ts,i - Tr,i);   /* For correctionField(i) calculation of Time Aware Bridges */
-         // 'Ts' means the synchronized time, maybe
-         // 'Ts,i - Tr,i' means the residence time
+   if(Type == Sync || Type == Pdelay_Req || Type == Pdelay_Resp) {
+   	
+      if(EthTSynHardwareTimestampSupport == true) {
+         /* the egress time stamp shall be retrieved for Pdelay_Req and Pdelay_Resp from the EthIf */
+         // EthIf_GetEgressTimeStamp(CtrlIdx, BufIdx, timeQualPtr, timeStampPtr);
+      } else {    // In case EthTSynHardwareTimestamp is set to FALSE
+         if(Type == Sync || Type == Pdelay_Resp) {
+            currentTime = StbM_GetCurrentTime(timeBaseId, timeStampPtr, userDataPtr);
+         } else if(Type == Pdelay_Req) {
+            currentTimeRaw = StbM_GetCurrentTimeRaw(timeStampRawPtr);
+             T1 = *timeStampRawPtr;
+         } else if(Type == Pdelay_Resp) {
+            givenTimeStamp = T2;
+            timeDifferenceOfCurrentTimeRaw = StbM_GetCurrentTimeDiff(givenTimeStamp, timeStampDiffPtr);
+            timeStampDiffPtr = (T3-T2)    /* One part of D = ((T4-T1) - (T3-T2)) / 2 */
+         } else if(Type == Sync && Type == EthTimeGatewayMasterPort) {
+            // givenTimeStamp = (Tr,i);   // Maybe 'Tr' means the time that received message, and 'i' means time-aware system indexed i
+            timeDifferenceOfCurrentTimeRaw = StbM_GetCurrentTimeDiff(givenTimeStamp, timeStampDiffPtr);
+            // timeStampDiffPtr = (Ts,i - Tr,i);   /* For correctionField(i) calculation of Time Aware Bridges */
+            // 'Ts' means the synchronized time, maybe
+            // 'Ts,i - Tr,i' means the residence time
+         }
       }
-   }
+   	}
 }
 
 Std_ReturnType	EthTSyn_TrcvLinkStateChg(uint8_t CtrlIdx, 
