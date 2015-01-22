b99ce807eeffe7998c8216c6bb54d3b7071aa6d6
WIDEN_SVN <zibets.han@gmail.com>
2015-01-22 15:53:36 +0900

    ethtsyn.c error exist. Can't compile now

--- net/stbm/ethtsyn.c
+++ net/stbm/ethtsyn.c
@@ -1,10 +1,18 @@
 
-
-#include <linux/stbm.h>
+#include <linux/types.h>
+#include <net/stbm.h>
 #include <net/ethtsyn.h>
 
+/* Initialize all internal variables and set the EthTSync module to init state */
 void 		EthTSyn_Init(const EthTSyn_ConfigType* configPtr) {
+  //When DET reporting is enabled EthTSyn module shall call DEt_ReportError() with the error code
+  //ETHTSYN_E_NOT_INITIALIZED when any API is called in uninitialized state
 
+  //After first initialized, when init function is called, reinitialize
+
+  //rate correction -> 0
+  //latency for ingress and egress to 0
+  
 }
 
 void 		EthTSyn_GetVersionInfo(Std_VersionInfoType* versioninfo) {
@@ -14,17 +22,17 @@ void 		EthTSyn_GetVersionInfo(Std_VersionInfoType* versioninfo) {
 Std_ReturnType 	EthTSyn_GetCurrentTime(StbM_SynchronizedTimeBaseType timeBaseId,
 				       StbM_TimeStampType* timeStampPtr,
 				       EthTSyn_SyncStateType* syncState) {
-
+  return E_OK;
 }
 
 Std_ReturnType 	EthTSyn_SetGlobalTime(StbM_SynchronizedTimeBaseType timeBaseId, 
 				      StbM_TimeStampType* timeStampPtr) {
-
+  return E_OK;
 }
 
 Std_ReturnType	EthTSyn_SetTransmissionMode(uint8_t CtrlIdx, 
 					    EthTSyn_TransmissionModeType Mode) {
-
+  return E_OK;
 }
 
 void		EthTSyn_RxIndication(uint8_t CtrlIdx,
@@ -38,15 +46,15 @@ void		EthTSyn_RxIndication(uint8_t CtrlIdx,
 
 void		EthTSyn_TxConfirmation(uint8_t CtrlIdx,
 				       uint8_t BufIdx) {
-
+ 
 }
 
-Std_ReturnType	EthTSyn_TrcvLinkStateChg(uint8 CtrlIdx, 
+Std_ReturnType	EthTSyn_TrcvLinkStateChg(uint8_t CtrlIdx, 
 					 EthTrcv_LinkStateType TrcvLinkState) {
-
+  return E_OK;
 }
 
 void 		EthTSyn_MainFunction(void) {
 
 }
-
+                                                                                                                                                                                                                                                                                                                                                                        
--- net/stbm/stbm.c
+++ net/stbm/stbm.c
@@ -1,13 +1,24 @@
 
 #include <linux/types.h>
+#include <linux/posix-clock.h>
 #include <net/stbm.h>
+
+/* StbMSynchronizedTimeBase */
+bool StbMIsSystemWideGlobalTimeMaster;
+enum StbMStoreTimebaseNonVolatile;
+uint32_t StbMSyncLossThreshold; 	/* Standard using floating point to present 0 to INF SEC(s) */
+uint32_t StbMSyncLossTimeout;		/* Standard using floating point to present 0 to INF SEC(s) */
+uint16_t StbMSynchronizedTimeBaseIdentifier;
 
 void StbM_GetVersionInfo(void) {
 
 }
 
 void StbM_Init(void) {
-
+  StbMIsSystemWideGlobalTimeMaster = 0;
+  StbMSyncLossThreshold = 0;
+  StbMSyncLossTimeout = 0;
+  StbMSynchronizedTimeBaseIdentifier = 0;
 }
 
 Std_ReturnType StbM_GetCurrentTime(StbM_SynchronizedTimeBaseType timeBaseId, 
@@ -23,18 +34,45 @@ Std_ReturnType StbM_GetCurrentTimeExtended(StbM_SynchronizedTimeBaseType timeBas
 }
 
 Std_ReturnType	StbM_GetCurrentTimeRaw(StbM_TimeStampRawType* timeStampRawPtr) {
+  
   return E_OK;
 }
 
 Std_ReturnType	StbM_GetCurrentTimeDiff(StbM_TimeStampRawType givenTimeStamp,
 					StbM_TimeStampRawType* timeStampDiffPtr) {
+  
+
   return E_OK;
 }
 
+//Service ID 0x0b
+//Allow the Customer to set the new global timer
+//Make this to be call by Application on UserSpace
 Std_ReturnType	StbM_SetGlobalTime(StbM_SynchronizedTimeBaseType timeBaseId, 
 				   StbM_TimeStampType* timeStampPtr,
 				   StbM_UserDataType* userDataPtr) {
+  struct timespec current_time;
+  
+  //init timeStampPtr
+  timeStampPtr->timeBaseStatus.TIMEOUT = 0;
+  timeStampPtr->timeBaseStatus.TIMELEAP = 0;
+  timeStampPtr->timeBaseStatus.SYNC_TO_GATEWAY = 0;
+  timeStampPtr->timeBaseStatus.GLOBAL_TIME_BASE = 1;
+  timeStampPtr->timeBaseStatus.rsv = 0;
+
+  getrawmonotonic(&current_time);
+
+  timeStampPtr->nanoseconds = current_time.tv_nsecs;
+  timeStampPtr->seconds = current_time.tv_sec;
+  timeStampPtr->secondsHi = 0;
+
+  
+
+  //if UserDataType is not null -> setting to where?
+  
   return E_OK;
+error:
+  return E_NOT_OK;
 }
 
 Std_ReturnType	StbM_SetUserData(StbM_SynchronizedTimeBaseType timeBaseId, 
