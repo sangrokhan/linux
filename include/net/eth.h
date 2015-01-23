8af9ffce6f795532c759a42d5a8ca20739b00671
WIDEN_SVN <zibets.han@gmail.com>
2015-01-23 20:18:19 +0900

    add comstack files, update to generaltypes.h

--- include/net/eth.h
+++ include/net/eth.h
@@ -42,65 +42,65 @@ extern bool	EthVersionInfoApi;
 
 
 
-extern void 		Eth_Init(const Eth_ConfigType* CfgPtr);
-extern Std_ReturnType	Eth_ControllerInit(uint8_t CtrlIdx, 
-					   uint8_t CfgIdx);
-extern Std_ReturnType	Eth_SetControllerMode(uint8_t CtrlIdx, 
-					      Eth_ModeType CtrlMode);
-extern Std_ReturnType	Eth_GetControllerMode(uint8_t CtrlIdx, 
-					      Eth_ModeType* CtrlModePtr);
-extern void		Eth_GetPhysAddr(uint8_t CtrlIdx, 
-					uint8_t* PhysAddrPtr);
-extern void 		Eth_SetPhysAddr(uint8_t CtrlIdx, 
-					const uint8_t* PhysAddrPtr);
-extern Std_ReturnType	Eth_UpdatePhyAddrFilter(uint8_t CtrlIdx, 
-						uint8_t* PhysAddrPtr, 
-						Eth_FilterActionType Action);
-extern Std_ReturnType	Eth_WriteMii(uint8_t CtrlIdx, 
-				     uint8_t TrcvIdx, 
-				     uint8_t RegIdx, 
-				     uint16_t RegVal);
-extern Std_ReturnType	Eth_ReadMii(uint8_t CtrlIdx, 
-				    uint8_t TrcvIdx, 
-				    uint8_t RegIdx, 
-				    uint16_t* RegValPtr);
-extern Std_ReturnType	Eth_GetDropCount(uint8_t CtrlIdx, 
-					 uint8_t CountValues, 
-					 uint32_t* DropCount);
-extern Std_ReturnType	Eth_GetEtherStats(uint8_t CtrlIdx, 
-					  uint32_t* etherStats);
-extern Std_ReturnType	Eth_GetCurrentTime(uint8_t CtrlIdx, 
-					   Eth_TimeStampQualType* timeQualPtr, 
-					   Eth_TimeStampType* timeStampPtr);
-extern void		Eth_EnableEgressTimeStamp(uint8_t CtrlIdx, 
-						  uint8_t BufIdx);
-extern void 		Eth_GetEgressTimeStamp(uint8_t CtrlIdx, 
-					       uint8_t BufIdx, 
-					       Eth_TimeStampQualType* timeQualPtr, 
-					       Eth_TimeStampType* timeStampPtr);
-extern void		Eth_GetIngressTimeStamp(uint8_t CtrlIdx, 
-						Eth_DataType* DataPtr, 
-						Eth_TimeStampQualType* timeQualPtr, 
-						Eth_TimeStampType* timeStampPtr);
-extern void		Eth_SetCorrectionTime(uint8_t CtrlIdx, 
-					      Eth_TimeIntDiffType* timeOffsetPtr, 
-					      Eth_RateRatioType* rateRatioPtr);
-extern Std_ReturnType	Eth_SetGlobalTime(uint8_t CtrlIdx, 
-					  Eth_TimeStampType* timeStampPtr);
-extern BufReq_ReturType Eth_ProvideTxBuffer(uint8_t CtrlIdx, 
-					    Eth_BufIdxType* BufIdxPtr, 
-					    uint8_t** BufPtr, 
-					    uint16_t* LenBytePtr);
-extern Std_ReturnType	Eth_Transmit(uint8_t CtrlIdx, 
-				     Eth_bufIdxType BufIdx, 
-				     Eth_FrameType FrameType, 
-				     bool TxConfirmation, 
-				     uint16_t LenByte, 
-				     uint8_t* PhysAddrPtr);
-extern void		Eth_Receive(uint8_t CtrlIdx, 
-				    Eth_RxStatusType* RxStatusPtr);
-extern void		Eth_TxConfirmation(uint8_t CtrlIdx);
-extern void		Eth_GetVersionInfo(Std_VersionInfoType* VersionInfoPtr);
-extern void		Eth_MainFunction(void);
+extern void 			Eth_Init(const Eth_ConfigType* CfgPtr);
+extern Std_ReturnType		Eth_ControllerInit(uint8_t CtrlIdx, 
+						   uint8_t CfgIdx);
+extern Std_ReturnType		Eth_SetControllerMode(uint8_t CtrlIdx, 
+						      Eth_ModeType CtrlMode);
+extern Std_ReturnType		Eth_GetControllerMode(uint8_t CtrlIdx, 
+						      Eth_ModeType* CtrlModePtr);
+extern void			Eth_GetPhysAddr(uint8_t CtrlIdx, 
+						uint8_t* PhysAddrPtr);
+extern void 			Eth_SetPhysAddr(uint8_t CtrlIdx, 
+						const uint8_t* PhysAddrPtr);
+extern Std_ReturnType		Eth_UpdatePhyAddrFilter(uint8_t CtrlIdx, 
+							uint8_t* PhysAddrPtr, 
+							Eth_FilterActionType Action);
+extern Std_ReturnType		Eth_WriteMii(uint8_t CtrlIdx, 
+					     uint8_t TrcvIdx, 
+					     uint8_t RegIdx, 
+					     uint16_t RegVal);
+extern Std_ReturnType		Eth_ReadMii(uint8_t CtrlIdx, 
+					    uint8_t TrcvIdx, 
+					    uint8_t RegIdx, 
+					    uint16_t* RegValPtr);
+extern Std_ReturnType		Eth_GetDropCount(uint8_t CtrlIdx, 
+						 uint8_t CountValues, 
+						 uint32_t* DropCount);
+extern Std_ReturnType		Eth_GetEtherStats(uint8_t CtrlIdx, 
+						  uint32_t* etherStats);
+extern Std_ReturnType		Eth_GetCurrentTime(uint8_t CtrlIdx, 
+						   Eth_TimeStampQualType* timeQualPtr, 
+						   Eth_TimeStampType* timeStampPtr);
+extern void			Eth_EnableEgressTimeStamp(uint8_t CtrlIdx, 
+							  uint8_t BufIdx);
+extern void 			Eth_GetEgressTimeStamp(uint8_t CtrlIdx, 
+						       uint8_t BufIdx, 
+						       Eth_TimeStampQualType* timeQualPtr, 
+						       Eth_TimeStampType* timeStampPtr);
+extern void			Eth_GetIngressTimeStamp(uint8_t CtrlIdx, 
+							Eth_DataType* DataPtr, 
+							Eth_TimeStampQualType* timeQualPtr, 
+							Eth_TimeStampType* timeStampPtr);
+extern void			Eth_SetCorrectionTime(uint8_t CtrlIdx, 
+						      Eth_TimeIntDiffType* timeOffsetPtr, 
+						      Eth_RateRatioType* rateRatioPtr);
+extern Std_ReturnType		Eth_SetGlobalTime(uint8_t CtrlIdx, 
+						  Eth_TimeStampType* timeStampPtr);
+extern BufReq_ReturnType 	Eth_ProvideTxBuffer(uint8_t CtrlIdx, 
+						    Eth_BufIdxType* BufIdxPtr, 
+						    uint8_t** BufPtr, 
+						    uint16_t* LenBytePtr);
+extern Std_ReturnType		Eth_Transmit(uint8_t CtrlIdx, 
+					     Eth_BufIdxType BufIdx, 
+					     Eth_FrameType FrameType, 
+					     bool TxConfirmation, 
+					     uint16_t LenByte, 
+					     uint8_t* PhysAddrPtr);
+extern void			Eth_Receive(uint8_t CtrlIdx, 
+					    Eth_RxStatusType* RxStatusPtr);
+extern void			Eth_TxConfirmation(uint8_t CtrlIdx);
+extern void			Eth_GetVersionInfo(Std_VersionInfoType* VersionInfoPtr);
+extern void			Eth_MainFunction(void);
 
 #endif
